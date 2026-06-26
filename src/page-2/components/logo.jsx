5100a957b838537ef2bf13bc382640e1125d64ac
Ei Than Thar Oo <2024-miit-cse-046@miit.edu.mm>
2026-06-26 23:08:03 +0630

    page 1:signup page,page2:login page and page3: get started page

---
+++ src/page-2/components/logo.jsx
@@ -0,0 +1,13 @@
+import './logo.css';
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img
+        src="/images/logo.png"
+        alt="Pixel Panel"
+        className="logo__icon logo__icon--full"
+      />
+    </div>
+  );
+}
